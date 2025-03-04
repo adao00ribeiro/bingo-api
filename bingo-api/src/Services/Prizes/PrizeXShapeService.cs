3d1536700a64c85004863b1e1f944ab5e3958c87
Adão Ribeiro <adao-eduardo@hotmail.com>
2025-03-04 14:13:42 -0300

    BNG-23: PrizeXShapeService

--- bingo-api/src/Services/Prizes/PrizeXShapeService.cs
+++ bingo-api/src/Services/Prizes/PrizeXShapeService.cs
@@ -3,48 +3,57 @@ using bingo_api.src.Interfaces.Repositories;
 
 namespace bingo_api.src.Services.Prizes;
 
-public class PrizeXShapeService : PrizeBaseService
-{
-    public PrizeXShapeService(Prize prize)
-       : base(prize) { }
-
-    protected override bool CheckWinner(Card card, int row, int col)
+  public class PrizeXShapeService : PrizeBaseService
     {
-        int size = (int)Math.Sqrt(card.CardMarkedNumbers.Length);
-        bool isWinner = true;
+        public PrizeXShapeService(Prize prize)
+            : base(prize) { }
 
-        for (int i = 0; i < size; i++)
+        protected override bool CheckWinner(Card card, int row, int col)
         {
-            if (card.CardMarkedNumbers[i * size + i] != 1 || card.CardMarkedNumbers[(i + 1) * (size - 1)] != 1)
+            if(row != col){
+                return false;
+            }
+            var isWinner = CheckXShape(card, col);
+            if (isWinner)
             {
-                isWinner = false;
-                break;
+                ExecuteTopFiveList(card, row, col);
+            }
+            return isWinner;
+        }
+
+        private bool CheckXShape(Card card, int col)
+        {
+            var matrix = card.CardMarkedNumbers.Chunk(col).ToList();
+            int rows = matrix.Count;
+
+            bool firstDiagonal = true;
+            bool secondDiagonal = true;
+
+            for (int i = 0; i < rows; i++)
+            {
+                if (matrix[i][i] != 1) firstDiagonal = false;
+                if (matrix[i][rows - 1 - i] != 1) secondDiagonal = false;
+            }
+
+            return firstDiagonal && secondDiagonal;
+        }
+
+        protected override void ExecuteTopFiveList(Card card, int row, int col)
+        {
+            var subNumbers = card.Numbers.Chunk(col).ToList();
+            var markedSubarrays = card.CardMarkedNumbers.Chunk(col).ToList();
+
+            for (int i = 0; i < subNumbers.Count; i++)
+            {
+                var subNumberArray = subNumbers[i];
+                var markedArray = markedSubarrays[i];
+
+                var markedNumbers = subNumberArray.Where((_, index) => markedArray[index] == 1).ToList();
+                var missingNumbers = subNumberArray.Except(markedNumbers).ToList();
+                var lackOfHits = missingNumbers.Count;
+
+                prize.SetTopFive(card, lackOfHits, missingNumbers);
             }
         }
-
-        if (isWinner)
-        {
-            ExecuteTopFiveList(card, row, col);
-        }
-        
-        return isWinner;
     }
 
-    protected override void ExecuteTopFiveList(Card card, int row, int col)
-    {
-        var markedIndices = new List<int>();
-        int size = (int)Math.Sqrt(card.CardMarkedNumbers.Length);
-
-        for (int i = 0; i < size; i++)
-        {
-            markedIndices.Add(i * size + i);
-            markedIndices.Add((i + 1) * (size - 1));
-        }
-
-        var markedNumbers = card.Numbers.Where((_, index) => markedIndices.Contains(index)).ToList();
-        var missingNumbers = card.Numbers.Except(markedNumbers).ToList();
-        var lackOfHits = missingNumbers.Count;
-
-        prize.SetTopFive(card, lackOfHits, missingNumbers);
-    }
-}
