06013519b4e50f5bf30a64e05667ef95845fd9b0
Adão Ribeiro <adao-eduardo@hotmail.com>
2025-06-26 09:34:24 -0300

    ajuste webhook

--- bingo-api/src/Controllers/WebHookController.cs
+++ bingo-api/src/Controllers/WebHookController.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Asp.Versioning;
 using bingo_api.src.Controllers.Shared;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace bingo_api.src.Controllers;
-
-
 [ApiVersion("1.0")]
 public class WebHookController : ApiControllerBase
 {
@@ -20,16 +13,12 @@ public class WebHookController : ApiControllerBase
         _logger = logger;
     }
 
-    [HttpPost]
-    public async Task<IActionResult> Receive()
+    [HttpPost("pushpay")]
+    public async Task<IActionResult> ReceivePushPayWebhook()
     {
         using var reader = new StreamReader(Request.Body);
         var body = await reader.ReadToEndAsync();
-
         _logger.LogInformation("Webhook recebido: {payload}", body);
-        // Você pode processar aqui os dados: verificar status, valor, etc.
-        // Exemplo: string status = payload.status;
-
         return Ok();
     }
 }
