d543f29f3b8c99efa38f70de5167a7eb69a75526
Adão Ribeiro <adao-eduardo@hotmail.com>
2025-06-27 09:45:52 -0300

    ajuste

--- bingo-api/src/Mappings/PunterMap.cs
+++ bingo-api/src/Mappings/PunterMap.cs
@@ -6,83 +6,86 @@ namespace bingo_api.src.Mappings;
 
 public class PunterMap : IEntityTypeConfiguration<Punter>
 {
-    public void Configure(EntityTypeBuilder<Punter> builder)
-    {
-        builder.ToTable("punters");
+       public void Configure(EntityTypeBuilder<Punter> builder)
+       {
+              builder.ToTable("punters");
 
-        builder.HasKey(x => x.Id);
+              builder.HasKey(x => x.Id);
 
-        builder.Property(x => x.Id)
-               .HasColumnName("id")
-               .ValueGeneratedOnAdd();
+              builder.Property(x => x.Id)
+                     .HasColumnName("id")
+                     .ValueGeneratedOnAdd();
 
-        builder.Property(x => x.CreateAt)
-               .HasColumnName("create_at")
-               .IsRequired()
-               .HasDefaultValueSql("CURRENT_TIMESTAMP");
+              builder.Property(x => x.CreateAt)
+                     .HasColumnName("create_at")
+                     .IsRequired()
+                     .HasDefaultValueSql("CURRENT_TIMESTAMP");
 
-        builder.Property(x => x.UpdateAt)
-               .HasColumnName("update_at")
-               .HasDefaultValueSql("CURRENT_TIMESTAMP");
+              builder.Property(x => x.UpdateAt)
+                     .HasColumnName("update_at")
+                     .HasDefaultValueSql("CURRENT_TIMESTAMP");
 
-        builder.Property(x => x.Balance)
-               .HasColumnName("balance")
-               .IsRequired()
-               .HasColumnType("numeric(15, 2)");
+              builder.Property(x => x.Balance)
+                     .HasColumnName("balance")
+                     .IsRequired()
+                     .HasColumnType("numeric(15, 2)");
 
-        builder.Property(x => x.PrizeBalance)
-               .HasColumnName("prize_balance")
-               .IsRequired()
-               .HasColumnType("numeric(15, 2)");
+              builder.Property(x => x.PrizeBalance)
+                     .HasColumnName("prize_balance")
+                     .IsRequired()
+                     .HasColumnType("numeric(15, 2)");
 
-        builder.Property(x => x.IsBot)
-               .HasColumnName("is_bot")
-               .IsRequired()
-               .HasDefaultValue(false);
+              builder.Property(x => x.IsBot)
+                     .HasColumnName("is_bot")
+                     .IsRequired()
+                     .HasDefaultValue(false);
 
-        builder.Property(x => x.Email)
-               .HasColumnName("email")
-               .HasMaxLength(100)
-               .IsRequired();
+              builder.Property(x => x.Email)
+                     .HasColumnName("email")
+                     .HasMaxLength(100)
+                     .IsRequired();
 
-        builder.Property(x => x.Name)
-               .HasColumnName("name")
-               .HasMaxLength(50)
-               .IsRequired();
+              builder.Property(x => x.Name)
+                     .HasColumnName("name")
+                     .HasMaxLength(50)
+                     .IsRequired();
 
-        builder.Property(x => x.IndicateTag)
-                .HasColumnName("indicate_tag")
-                .HasMaxLength(50);
+              builder.Property(x => x.IndicateTag)
+                      .HasColumnName("indicate_tag")
+                      .HasMaxLength(50);
 
-        builder.Property(x => x.Cpf)
-               .HasColumnName("cpf")
-               .HasMaxLength(11)
-               .IsRequired();
+              builder.Property(x => x.Cpf)
+                     .HasColumnName("cpf")
+                     .HasMaxLength(11)
+                     .IsRequired();
 
-        builder.Property(x => x.DateBirth)
-               .HasColumnName("date_birth")
-               .IsRequired();
+              builder.Property(x => x.DateBirth)
+                     .HasColumnName("date_birth")
+                     .IsRequired();
 
-        builder.Property(x => x.SellerId)
-               .HasColumnName("seller_id");
+              builder.Property(x => x.SellerId)
+                     .HasColumnName("seller_id");
 
-        builder.HasIndex(x => x.Email)
-               .IsUnique()
-               .HasDatabaseName("ux_punters_email");
+              builder.HasIndex(x => x.Email)
+                     .IsUnique()
+                     .HasDatabaseName("ux_punters_email");
 
-        builder.HasMany(p => p.Cards)
-               .WithOne(c => c.Punter)
-               .HasForeignKey(c => c.PunterId)
-               .OnDelete(DeleteBehavior.Cascade);
+              builder.HasMany(p => p.Cards)
+                     .WithOne(c => c.Punter)
+                     .HasForeignKey(c => c.PunterId)
+                     .OnDelete(DeleteBehavior.Cascade);
 
-        builder.HasMany(p => p.Recharges)
-               .WithOne(r => r.Punter)
-               .HasForeignKey(r => r.PunterId)
-               .OnDelete(DeleteBehavior.Cascade);
+              builder.HasMany(p => p.Recharges)
+                     .WithOne(r => r.Punter)
+                     .HasForeignKey(r => r.PunterId)
+                     .OnDelete(DeleteBehavior.Cascade);
 
-        builder.HasOne(p => p.Seller)
-               .WithMany(s => s.Punters)
-               .HasForeignKey(p => p.SellerId)
-               .OnDelete(DeleteBehavior.Restrict);
+              builder.HasOne(p => p.Seller)
+                     .WithMany(s => s.Punters)
+                     .HasForeignKey(p => p.SellerId)
+                     .OnDelete(DeleteBehavior.Restrict);
+
+               builder.HasIndex(x => x.Cpf)
+              .IsUnique();
     }
 }
