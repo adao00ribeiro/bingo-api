4de32caf9d20ccc02083475f3b0aa186c343d0f1
Adão Ribeiro <adao-eduardo@hotmail.com>
2026-03-19 21:51:32 -0300

    AJUSTE

--- bingo-api/src/DTOs/Response/Scratch/ScratchGameOverrideResponseDto.cs
+++ bingo-api/src/DTOs/Response/Scratch/ScratchGameOverrideResponseDto.cs
@@ -1,11 +1,10 @@
+using bingo_api.src.DTOs.Shared;
 using bingo_api.src.Entities.Scratch;
 
 namespace bingo_api.src.DTOs.Response.Scratch;
 
-public record ScratchGameOverrideResponseDto
+public record ScratchGameOverrideResponseDto : EntityResponseDto
 {
-    public Guid Id { get; set; }
-
     public string Title { get; set; }
 
     public string Subtitle { get; set; }
@@ -16,19 +15,38 @@ public record ScratchGameOverrideResponseDto
 
     public Guid ScratchGameId { get; set; }
 
-    public string? ScratchGameName { get; set; }
+
+    public ScratchGameOverrideResponseDto(
+        Guid id,
+      string title,
+      string subtitle,
+       decimal cardValue,
+        Guid onlineHouseId,
+        Guid scratchGameId,
+          DateTime CreatedAt,
+      DateTime UpdatedAt
+        ) : base(id, CreatedAt, UpdatedAt)
+    {
+        Title = title;
+        Subtitle = subtitle;
+        CardValue = cardValue;
+        OnlineHouseId = onlineHouseId;
+        ScratchGameId = scratchGameId;
+    }
 
     public static ScratchGameOverrideResponseDto ConvertToDto(ScratchGameOverride entity)
     {
-        return new ScratchGameOverrideResponseDto
-        {
-            Id = entity.Id,
-            Title = entity.Title,
-            Subtitle = entity.Subtitle,
-            CardValue = entity.CardValue,
-            OnlineHouseId = entity.OnlineHouseId,
-            ScratchGameId = entity.ScratchGameId,
-            ScratchGameName = entity.ScratchGame?.Name
-        };
+        return new ScratchGameOverrideResponseDto(
+            
+            id:  entity.Id,
+            title : entity.Title,
+            subtitle : entity.Subtitle,
+            cardValue : entity.CardValue,
+            onlineHouseId : entity.OnlineHouseId,
+            scratchGameId : entity.ScratchGameId,
+            CreatedAt : entity.CreatedAt,
+            UpdatedAt: entity.UpdatedAt
+        );
+      
     }
 }
